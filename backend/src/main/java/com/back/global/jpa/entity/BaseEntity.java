46799399655fa88bf6b880a8838c3dfcc52a0577
1J-Choi <chldnjswp110@naver.com>
2025-09-30 16:14:14 +0900

    ✨ Feat[BE](project): project 엔티티 및 기본 세팅 설정

--- backend/src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ backend/src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
 
     @CreatedDate
     private LocalDateTime createDate;
